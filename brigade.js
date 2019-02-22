065852dbcbda78fcbbf5c65a630d0eac4e6a5973
rahul_abrol <rahul.abrol@successive.tech>
2019-02-22 12:17:33 +0530

    Added brigade

--- brigade.js
+++ brigade.js
@@ -4,7 +4,8 @@ const { events, Job } = require("brigadier");
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.revision.commit)
 
-  var nodeBuild = new Job("node-Build","node:10.15.0-slim")
+  var nodeBuild = new Job("nodeBuild")
+  nodeBuild.image = "node:10.15.0-slim"
 
   nodeBuild.tasks = [
     "cd /src/", 
