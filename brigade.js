2791e80ff7bbbbfd1abbb9d6224a4686a3ffc072
rahul_abrol <rahul.abrol@successive.tech>
2019-03-18 14:06:31 +0530

    Triggered Build

--- brigade.js
+++ brigade.js
@@ -13,7 +13,7 @@ class JobFactory {
         "cd /src",
         `echo -n '{"type": "${project.secrets.type}","project_id": "${project.secrets.project_id}","private_key_id": "${project.secrets.private_key_id}","private_key": "${project.secrets.private_key}","client_email": "${project.secrets.client_email}","client_id": "${project.secrets.client_id}","auth_uri": "${project.secrets.auth_uri}","token_uri": "${project.secrets.token_uri}","auth_provider_x509_cert_url": "${project.secrets.auth_provider_x509_cert_url}","client_x509_cert_url": "${project.secrets.client_x509_cert_url}"}' > file.json`,
         "ls -lart",
-        `echo -n "${project.secrets.private_key}"`,
+        `echo -n '"${project.secrets.private_key}"'`,
 //        "cat file.json",
         `gcloud auth activate-service-account ${project.secrets.client_email} --key-file=file.json`,
         `gcloud config set project ${project.secrets.project_id}`,
