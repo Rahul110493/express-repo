4b66c4d8581edba100313e0b336ef7d1279ed190
rahul_abrol <rahul.abrol@successive.tech>
2019-03-18 22:49:34 +0530

    Updated

--- brigade.js
+++ brigade.js
@@ -12,8 +12,9 @@ class JobFactory {
        buildcloud.tasks = [
         "cd /src",
 //        `echo -e '{"type": "${project.secrets.type}","project_id": "${project.secrets.project_id}","private_key_id": "${project.secrets.private_key_id}","private_key": "${project.secrets.private_key}","client_email": "${project.secrets.client_email}","client_id": "${project.secrets.client_id}","auth_uri": "${project.secrets.auth_uri}","token_uri": "${project.secrets.token_uri}","auth_provider_x509_cert_url": "${project.secrets.auth_provider_x509_cert_url}","client_x509_cert_url": "${project.secrets.client_x509_cert_url}"}' > file.json`,
-        "ls -lart",
+        
         `echo '"${project.secrets.private_key}"'||sed ':a;N;$!ba;s/\n/\\n/g >file.json'`,
+        "ls -lart",
         "cat file.json"
 //        "cat file.json",
 //        "echo  $PRIVATE_KEY"
