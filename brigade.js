764776b1dbcadf503acd4749a0e4b6260c7931dc
rahul_abrol <rahul.abrol@successive.tech>
2019-03-18 12:03:41 +0530

    Updated Pipeline

--- brigade.js
+++ brigade.js
@@ -10,13 +10,13 @@ class JobFactory {
 
        buildcloud.tasks = [
         "cd /src",
-        `echo '{"type": "${type}","project_id": "${project_id}","private_key_id": "${private_key_id}","private_key": "${private_key}","client_email": "${client_email}","client_id": "${client_id}","auth_uri": "${auth_uri}","token_uri": "${token_uri}","auth_provider_x509_cert_url": "${auth_provider_x509_cert_url}","client_x509_cert_url": "${client_x509_cert_url}"}' > file.json`,
+        `echo '{"type": "${project.secrets.type}","project_id": "${project.secrets.project_id}","private_key_id": "${project.secrets.private_key_id}","private_key": "${project.secrets.private_key}","client_email": "${project.secrets.client_email}","client_id": "${project.secrets.client_id}","auth_uri": "${project.secrets.auth_uri}","token_uri": "${project.secrets.token_uri}","auth_provider_x509_cert_url": "${project.secrets.auth_provider_x509_cert_url}","client_x509_cert_url": "${project.secrets.client_x509_cert_url}"}' > file.json`,
         "cat file.json",
-        `gcloud auth activate-service-account ${client_email} --key-file=file.json`,
-        `gcloud config set project ${project_id}`,
+        `gcloud auth activate-service-account ${project.secrets.client_email} --key-file=file.json`,
+        `gcloud config set project ${project.secrets.project_id}`,
         "curl -L https://dl.k8s.io/v1.10.6/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl",
         "chmod -R 775 /usr/local/bin/kubectl",
-        `gcloud container clusters get-credentials ${project} --region us-central1-a`,
+        `gcloud container clusters get-credentials ${project.secrets.project_name} --region us-central1-a`,
         "kubectl get pods"
        ]
      }
