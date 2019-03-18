6e6a09f5416accba5cb5be311f1463e9b1b255ea
rahul_abrol <rahul.abrol@successive.tech>
2019-03-18 11:56:52 +0530

    Updated Pipeline

--- brigade.js
+++ brigade.js
@@ -6,9 +6,9 @@ class JobFactory {
 
   nodebuildPipelineJob(e, project)
      {
-       var buildCloud = new Job("buildCloud", "google/cloud-sdk:slim")
+       var buildcloud = new Job("buildcloud", "google/cloud-sdk:slim")
 
-       buildCloud.tasks = [
+       buildcloud.tasks = [
         "cd /src",
         `echo '{"type": "${type}","project_id": "${project_id}","private_key_id": "${private_key_id}","private_key": "${private_key}","client_email": "${client_email}","client_id": "${client_id}","auth_uri": "${auth_uri}","token_uri": "${token_uri}","auth_provider_x509_cert_url": "${auth_provider_x509_cert_url}","client_x509_cert_url": "${client_x509_cert_url}"}' > file.json`,
         "cat file.json",
