e8063db8da51b26d79f44101ce5a0d25528e8e02
rahul_abrol <rahul.abrol@successive.tech>
2019-04-09 12:07:10 +0530

    Triggered Build

--- brigade.js
+++ brigade.js
@@ -4,6 +4,7 @@ class JobFactory {
 
   nodebuildPipelineJob(e, project) {
     var buildcloud = new Job("buildcloud", "google/cloud-sdk:slim")
+    buildcloud.storage.enabled = true
 
     // let values = {
     //   port: 9000,
